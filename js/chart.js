2f04d0a0f38fb6fc92fe98384868f547040f486a
addyas <ashleydyas92@gmail.com>
2016-10-06 03:36:34 -0700

    add some content

---
+++ js/chart.js
@@ -0,0 +1,29 @@
+google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Expertise', 'Percentage'],
+          ['Interaction Design',   35],
+          ['Development',      30],
+          ['UX',  20],
+          ['Visual Design', 15]
+        ]);
+
+        var options = {
+          title: 'What I do',
+          pieHole: 0.6,
+          backgroundColor: {fill: "transparent"},
+          height: "300",
+          width: "400",
+          chartArea: {left: 15, top: 0, width: '100%', height: '100%'},
+          legend: {alignment: 'center', textStyle: {color: "#757575", fontSize: "17", fontName: "Abel"}},
+          colors: ["#AD1457","#121212","#757575","#D84315"],
+          pieSliceBorderColor: "#757575",
+          pieSliceText: "none",
+          tooltip: {text: "percentage"}
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+
+        chart.draw(data, options);
+      }
