afa372d87983d0ae9af7635a2133eca9a157ff03
addyas <ashleydyas92@gmail.com>
2016-10-06 16:11:04 -0700

    responsive testing

--- js/chart.js
+++ js/chart.js
@@ -1,33 +1,41 @@
-google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Expertise', 'Percentage'],
-          ['Interaction Design',   35],
-          ['Development',      30],
-          ['UX',  20],
-          ['Visual Design', 15]
-        ]);
+$(document).ready(function(){
+  google.charts.load("current", {packages:["corechart"]});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+          var data = google.visualization.arrayToDataTable([
+            ['Expertise', 'Percentage'],
+            ['Interaction Design',   35],
+            ['Development',      30],
+            ['UX',  20],
+            ['Visual Design', 15]
+          ]);
 
-        var options = {
-          title: 'What I do',
-          pieHole: 0.6,
-          backgroundColor: {fill: "transparent"},
-          height: "100%",
-          width: "100%",
-          chartArea: {left: 15, top: 0, width: '100%', height: '100%'},
-          legend: {alignment: 'center',textStyle: {color: "#757575", fontSize: "17", fontName: "Abel"}},
-          colors: ["#757575","#121212","#D84315","#AD1457"],
-          pieSliceBorderColor: "#757575",
-          pieSliceText: "none",
-          tooltip: {text: "percentage"}
-        };
+          var options = {
+            title: 'What I do',
+            pieHole: 0.6,
+            backgroundColor: {fill: "transparent"},
+            height: "100%",
+            width: "100%",
+            chartArea: {left: 15, top: 0, width: '100%', height: '100%'},
+            legend: {alignment: 'center',textStyle: {color: "#757575", fontSize: "17", fontName: "Abel"}},
+            colors: ["#757575","#121212","#D84315","#AD1457"],
+            pieSliceBorderColor: "#757575",
+            pieSliceText: "none",
+            tooltip: {text: "percentage"}
+          };
 
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+          var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
 
-        chart.draw(data, options);
-      }
+          chart.draw(data, options);
+        }
 
-      $(window).resize(function(){
-         drawChart();
-      });
+        $(window).resize(function(){
+           drawChart();
+           console.log('chart should be redrawn');
+        });
+
+
+
+
+
+});
